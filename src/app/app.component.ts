b0dec3ba64c57b7eab5c4e67bbc169a98c499499
Anil Kumar <ipec.anil2@outlook.com>
2023-02-16 23:13:32 +0530

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'URLDecoderAndEncoder';
+  title = 'URL Decoder And Encoder';
+  
+  decode(inputArea: HTMLTextAreaElement, outputArea: HTMLTextAreaElement) {
+    outputArea.value = decodeURIComponent(inputArea.value);
+  }
+
+  encode(inputArea: HTMLTextAreaElement, outputArea: HTMLTextAreaElement) {
+    outputArea.value = encodeURIComponent(inputArea.value);
+  }
+
+  copyToClipboard(outputArea: HTMLTextAreaElement) {
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(outputArea.value).then(() => {
+        alert('Copied to Clipboard')
+      }, (error) => {
+        console.log(error)
+      });
+    } else {
+      console.log('Browser do not support Clipboard API')
+    }
+  }
 }
